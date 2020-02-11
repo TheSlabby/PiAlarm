91614d48d8b1d1603a27d22f0003f35b0b226430
Walker McGilvary <walker.mcgilvary@gmail.com>
2020-02-10 23:40:17 -0600

    added file saving and loading

--- alarm.py
+++ alarm.py
@@ -1,4 +1,5 @@
-import sched,time,sys,datetime
+import sched,time,sys,datetime,json,os.path
+from os import path
 from multiprocessing import Process, Queue, Manager, Value
 import pygame
 
@@ -8,25 +9,44 @@ manager = Manager()
 alarms = manager.list()
 alarmObjects = []
 scheduler = sched.scheduler(time.time, time.sleep)
+alarmsFile = 'alarms.json'
 
 class Alarm:
     currentID = 0
-    def __init__(self, name, time, sound):
+    def __init__(self, name, time, sound,ID=None):
         self.goal = datetime.datetime.strptime(time, timeFormat) #test code
+        if not path.exists('Sounds/'+sound):
+            raise
         Alarm.currentID += 1
         self.ID = Alarm.currentID
+        if ID: #code mostly unneccesary
+            self.ID = ID
+            if ID > currentID:
+                currentID = ID
         self.sound = sound
         self.time = time
         self.name = name
         self.process = Process(target=self.runTimer)
         self.process.start()
         
+        #add to lists if successful
+        alarms.append(Alarm.currentID)
+        alarmObjects.append(self)
+        
+        
+    def decode(self):
+        struct = {}
+        struct['time'] = self.time
+        struct['name'] = self.name
+        struct['ID'] = self.ID
+        struct['sound'] = self.sound
+        return struct
+        
     def runTimer(self):
         while self.ID in alarms:
             #get delta time
             now = datetime.datetime.now()
             seconds = (datetime.timedelta(hours=24) - (now - self.goal)).total_seconds() % (24 * 3600)
-            print(seconds)
             scheduler.enter(seconds, 1, self.alarm)
             scheduler.run()
     
@@ -35,7 +55,6 @@ class Alarm:
             pygame.mixer.init()
             pygame.mixer.music.load('Sounds/'+self.sound)
             pygame.mixer.music.play()
-            print('alarm!')
         else:
             p('Alarm no longer exists!')
 
@@ -45,30 +64,64 @@ def p(text):
 def i(text):
     return input(text+' > ')
 
+def tryCreateAlarm(name,time,sound,ID=None):
+    try:
+        alarm = Alarm(name,time,sound,ID)
+        p('Added '+alarm.name+' to queue with ID: '+str(alarm.ID) + '!')
+    except:
+        p('Invalid alarm!')
+
+
+def clearAlarms():
+    p('Clearing alarms...')
+    alarms[:] = []
+    alarmObjects.clear()
+
+def save():
+    f = open(alarmsFile, 'w')
+    arr = []
+    for a in alarmObjects:
+        struct = a.decode()
+        arr.append(struct)
+    data = json.dumps(arr)
+    p('Saving data to file: '+data)
+    f.write(data)
+
+def load():
+    clearAlarms()
+    f = open(alarmsFile, 'r')
+    data = json.loads(f.read())
+    for alarm in data:
+        tryCreateAlarm(alarm['name'],alarm['time'],alarm['sound'])
+    p('Finished loading!')
+
 while True:
     cmd = input(shellPrefix)
     if cmd == 'stop' or cmd == 'exit':
+        if i('Save?') == 'y':
+            save()
         p('Stopping..')
-        exit()
+        sys.exit()
     elif cmd == 'new':
         time = i('Time of alarm (format: '+timeFormat+'?')
-        name = i('Name of alarm?')
+        while True:
+            name = i('Name of alarm?')
+            duplicate = False
+            for alarm in alarmObjects:
+                if name == alarm.name: duplicate = True
+            if not duplicate: break
+            p('Name already taken!')
         sound = i('Sound of alarm?')
-        try:
-            alarm = Alarm(name,time,sound)
-            alarms.append(Alarm.currentID)
-            alarmObjects.append(alarm)
-            p('Added '+alarm.name+' to queue with ID: '+str(alarm.ID) + '!')
-        except:
-            p('Invalid alarm!')
-    elif cmd == 'delete':
+        tryCreateAlarm(name,time,sound)
+    elif cmd == 'delete' or cmd == 'remove':
         name = i('Name of alarm to delete?')
         removed = False
         for alarm in alarmObjects:
-            if alarm.ID in alarms:
-                alarms.remove(alarm.ID)
-                alarmObjects.remove(alarm)
-                removed = True
+            if alarm.name == name:
+                if alarm.ID in alarms:
+                    alarms.remove(alarm.ID)
+                    alarmObjects.remove(alarm)
+                    removed = True
         if removed:
             p('Removed alarm')
         else:
@@ -77,5 +130,11 @@ while True:
         p('Listing '+str(len(alarmObjects))+' alarms:')
         for alarm in alarmObjects:
             print('Name: ' + alarm.name + '\tTime: '+alarm.time+'\tID:'+str(alarm.ID))
+    elif cmd == 'save':
+        save()
+    elif cmd == 'load':
+        load()
+    elif cmd == 'clear':
+        clearAlarms()
     else:
         p('Invalid command')
