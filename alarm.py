41f1da9dc84694e5214ffb785acd2bc305f509bc
Walker McGilvary <walker.mcgilvary@gmail.com>
2020-02-10 21:55:38 -0600

    functional prototype

--- alarm.py
+++ alarm.py
@@ -1,58 +1,80 @@
-import schedule,time
-from multiprocessing import Process, Queue
+import sched,time,sys,datetime
+from multiprocessing import Process, Queue, Manager, Value
 import pygame
 pygame.mixer.init()
 
-
-def job():
-    print("I'm working...")
-
+shellPrefix = '[PiAlarm Shell] '
+timeFormat = '%I:%M%p'
+manager = Manager()
+alarms = manager.list()
+alarmObjects = []
+scheduler = sched.scheduler(time.time, time.sleep)
 
 class Alarm:
+    currentID = 0
     def __init__(self, name, time, sound):
-        # MOVE THIS TO self.setup - self.job = schedule.every().day.at(time).do(self.alarm)
-	#SETUP WILL BE CALLED FROM THE OTHER THREAD
-        pygame.mixer.music.load('Sounds/' + sound)
+        self.goal = datetime.datetime.strptime(time, timeFormat) #test code
+        Alarm.currentID += 1
+        self.ID = Alarm.currentID
         self.sound = sound
         self.time = time
         self.name = name
+        self.process = Process(target=self.runTimer)
+        self.process.start()
+        
+    def runTimer(self):
+        while self.ID in alarms:
+            #get delta time
+            now = datetime.datetime.now()
+            seconds = (datetime.timedelta(hours=24) - (now - self.goal)).total_seconds() % (24 * 3600)
+            scheduler.enter(seconds, 1, self.alarm)
+            scheduler.run()
+    
     def alarm(self):
-        pygame.mixer.music.load('Sounds/'+self.sound)
-        pygame.mixer.music.play()
-        print('alarm!')
+        if self.ID in alarms:
+            pygame.mixer.music.load('Sounds/'+self.sound)
+            pygame.mixer.music.play()
+            print('alarm!')
+        else:
+            p('Alarm no longer exists!')
 
-alarms = []
-q = Queue()
 
 def p(text):
     print('PiAlarm: '+text)
 def i(text):
     return input(text+' > ')
 
-def clockLoop():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-        print(repr(schedule.default_scheduler.jobs))
-
-clockProcess = Process(target=clockLoop)
-clockProcess.start()
-
 while True:
-    cmd = input('[PiAlarm Shell] ')
-    if cmd == 'stop':
+    cmd = input(shellPrefix)
+    if cmd == 'stop' or cmd == 'exit':
         p('Stopping..')
         exit()
     elif cmd == 'new':
-        t = i('Time of alarm?')
-        n = i('Name of alarm?')
-        s = i('Sound of alarm?')
+        time = i('Time of alarm (format: '+timeFormat+'?')
+        name = i('Name of alarm?')
+        sound = i('Sound of alarm?')
         try:
-            alarm = Alarm(n,t,s)
-            alarms.append(alarm)
+            alarm = Alarm(name,time,sound)
+            alarms.append(Alarm.currentID)
+            alarmObjects.append(alarm)
+            p('Added '+alarm.name+' to queue with ID: '+str(alarm.ID) + '!')
         except:
             p('Invalid alarm!')
-    elif cmd == 'run all':
-        schedule.default_scheduler.run_all()
+    elif cmd == 'delete':
+        name = i('Name of alarm to delete?')
+        removed = False
+        for alarm in alarmObjects:
+            if alarm.ID in alarms:
+                alarms.remove(alarm.ID)
+                alarmObjects.remove(alarm)
+                removed = True
+        if removed:
+            p('Removed alarm')
+        else:
+            p("Couldn't find alarm!")
+    elif cmd == 'list':
+        p('Listing '+str(len(alarmObjects))+' alarms:')
+        for alarm in alarmObjects:
+            print('Name: ' + alarm.name + '\tTime: '+alarm.time+'\tID:'+str(alarm.ID))
     else:
         p('Invalid command')
